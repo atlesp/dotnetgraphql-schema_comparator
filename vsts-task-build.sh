91584b3d4b627300cee2d6fb8ed36fcc11d8bcd8
Atle Esperum <me@esperum.no>
2018-10-15 15:10:06 +0200

    playing with versioning

--- vsts-task-build.sh
+++ vsts-task-build.sh
@@ -1,6 +1,8 @@
+
 #copy dlls for the comperator
 cp ./schema_comparator_cli/bin/Release/*  ./vsts_task/GraphqlSchemaComperator/comperator
 #update version numbers
 
 #build the extension 
-tfx extension create --manifest-globs vss-extension.json
+tfx extension create --root ./vsts_task --manifest-globs vss-extension.json --rev-version
+
