2000d0d722a96ec109240b80f66d8cef1f4b7f33
Atle Esperum <me@esperum.no>
2018-10-16 07:57:40 +0200

    Bumping patch instead of minor when bumping version

--- vsts-task-build.sh
+++ vsts-task-build.sh
@@ -6,7 +6,8 @@
 #copy dlls for the comperator
 cp ./schema_comparator_cli/bin/Release/*  ./vsts_task/GraphqlSchemaComperator/comperator
 #update version numbers
-bumped release minor
+bumped release patch
 #build the extension 
 tfx extension create --root ./vsts_task --manifest-globs vss-extension.json --rev-version
+#tfx extension publish --root ./vsts_task --manifest-globs vss-extension.json --rev-version
 
