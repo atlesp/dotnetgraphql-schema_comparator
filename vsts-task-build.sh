848c9d8223b3651e0605f42b99d63bcfccdc0dee
Atle Esperum <me@esperum.no>
2018-10-16 07:46:39 +0200

    Testing out bumped for versioning

--- vsts-task-build.sh
+++ vsts-task-build.sh
@@ -1,8 +1,12 @@
 
+# build 
+
+#run tests
+
 #copy dlls for the comperator
 cp ./schema_comparator_cli/bin/Release/*  ./vsts_task/GraphqlSchemaComperator/comperator
 #update version numbers
-
+bumped release minor
 #build the extension 
 tfx extension create --root ./vsts_task --manifest-globs vss-extension.json --rev-version
 
